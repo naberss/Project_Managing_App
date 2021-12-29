1db6a5b03ac6ea03b3f1ccd6c5e3ede3f79a88a4
lucas <lucas-berto@hotmail.com>
2021-12-28 21:40:59 -0300

    [#1] - Commit Inicial
    *Apenas os arquivos base do Spring Initialzr.

---
+++ src/main/java/com/study/Project_Managin_App/ProjectManaginAppApplication.java
@@ -0,0 +1,13 @@
+package com.study.Project_Managin_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManaginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManaginAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/Project_Managin_App/ProjectManaginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Project_Managin_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManaginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
